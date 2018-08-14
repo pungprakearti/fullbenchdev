469b97a41f32566f99073b204646cf67bd19bc56
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-08-14 16:04:25 -0700

    Worked on gameplay mechanics.

--- memory/memory.js
+++ memory/memory.js
@@ -46,8 +46,8 @@ window.onload = function() {
 
   shuffle(deckArr);
 
+  //create cards
   for (let image of deckArr) {
-    //create cards
     var divCard = document.createElement('div');
     divCard.setAttribute('class', 'card');
 
@@ -65,16 +65,92 @@ window.onload = function() {
     divCard.appendChild(img);
   }
 
+  var selected1, selected2;
+
   //select first image
-  cardsContainer = document.querySelector('.cardsContainer');
+  var cardsContainer = document.querySelector('.cardsContainer');
   cardsContainer.addEventListener('click', function(event) {
-    console.log(event.target);
-    //cardBack to 0 opacity
-    event.target.parentElement.firstChild.setAttribute('opacity', '0');
-    //img to 1 opacity, visible transform scale(1.5) and z-index 5
-    event.target.parentElement.lastChild.setAttribute('opacity', '1');
-    event.target.parentElement.lastChild.setAttribute('visibility', 'visible');
-    event.target.parentElement.lastChild.setAttribute('scale', '1.5');
-    event.target.parentElement.lastChild.setAttribute('z-index', '5');
+    //select first image
+    if (selected1 === undefined) {
+      selected1 = event.target.parentElement.lastChild;
+      console.log(selected1);
+      showImage(event);
+    }
+
+    //select second image
+    if (selected1 !== undefined && selected2 === undefined) {
+      if (selected1 !== event.target.parentElement.lastChild) {
+        selected2 = event.target.parentElement.lastChild;
+        console.log(selected2);
+        showImage(event);
+      }
+    }
+
+    //delay
   });
+
+  function showImage(event) {
+    //cardBack to 0 opacity
+    event.target.parentElement.firstChild.style.opacity = 0;
+
+    //img to 1 opacity, visible transform scale(1.5) and z-index 5
+    event.target.parentElement.lastChild.style.opacity = 1;
+    event.target.parentElement.lastChild.style.visibility = 'visible';
+    event.target.parentElement.lastChild.style.transform = 'scale(1.5)';
+    event.target.parentElement.lastChild.style.zIndex = 5;
+    console.log('showed image');
+  }
+
+  function hideImagesShowCardBack(event) {
+    let imgArr = document.querySelectorAll('img');
+    for (let item of imgArr) {
+      if (item.style.zIndex == 5) {
+        // hide image
+        event.target.parentElement.lastChild.style.opacity = 0;
+        event.target.parentElement.lastChild.style.visibility = 'hidden';
+        event.target.parentElement.lastChild.style.transform = 'scale(1)';
+        event.target.parentElement.lastChild.style.zIndex = 1;
+
+        // show card back
+        event.target.parentElement.firstChild.style.opacity = 1;
+        console.log('hide images');
+      }
+    }
+  }
+
+  function hideAll(event) {
+    let imgArr = document.querySelectorAll('img');
+    for (let item of imgArr) {
+      if (item.style.zIndex == 5) {
+        // hide image
+        event.target.parentElement.lastChild.style.visibility = 'hidden';
+
+        // hide card back
+        event.target.parentElement.firstChild.style.visibility = 'hidden';
+        console.log('hide images and hide card back');
+      }
+    }
+  }
 };
+
+// if selected1 = -1
+// if querySelector = cardback
+// selected1 = querySelector
+//scale image hide cardback
+
+//if selected2 = -1
+// if querySelector = cardback
+//selected2 = querySelector
+//scale image hide cardback
+
+//delay
+
+//MATCH
+//if selected1 = selected2
+//match++
+//if match >= 15 game over
+//else hide images
+
+//MISS
+//selected1 and selected2 = -1;
+// shrink and hide images, unhind cardback
