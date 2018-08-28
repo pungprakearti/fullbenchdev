2843511ece27714e8946502d211b7558a1a003d5
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-08-28 10:52:16 -0700

    Tinkering with js for sticky navbar

--- test/test.js
+++ test/test.js
@@ -1,19 +1,22 @@
-window.onload = function() {
-  //sticky nav bar
-  window.onscroll = function() {
-    myFunction();
-  };
+//sticky nav bar
+var navBar = document.querySelector('.navBar');
+var sticky = navBar.offsetTop;
 
-  var navbar = document.querySelector('.navBar');
-  var sticky = navbar.offsetTop;
-  console.log(navbar);
+window.onscroll = stickyNavBar();
+console.log(navBar.offsetTop);
+console.log(window.pageYOffset);
 
-  function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add('sticky');
-    } else {
-      navbar.classList.remove('sticky');
-    }
+function stickyNavBar() {
+  console.log('working here');
+  if (window.pageYOffset >= sticky) {
+    // navBar.classList.add('sticky');
+    navBar.style.position = 'fixed';
+    navBar.style.top = 0;
+    console.log('setting attributes');
+  } else {
+    navBar.style.position = 'relative';
+    navBar.style.top = '240px';
   }
-};
+}
+
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_sticky
