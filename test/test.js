fe73c7c815244ea73a0acb78bf944309773a6e9d
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-08-29 16:19:58 -0700

    sticky nav bar doesn't work with hiding scroll bar for parallax effect

--- test/test.js
+++ test/test.js
@@ -1,22 +1,12 @@
-//sticky nav bar
+//sticky nav bar doesn't work when hiding scroll bar
 var navBar = document.querySelector('.navBar');
-var sticky = navBar.offsetTop;
 
-window.onscroll = stickyNavBar();
-console.log(navBar.offsetTop);
-console.log(window.pageYOffset);
-
-function stickyNavBar() {
-  console.log('working here');
-  if (window.pageYOffset >= sticky) {
-    // navBar.classList.add('sticky');
-    navBar.style.position = 'fixed';
-    navBar.style.top = 0;
-    console.log('setting attributes');
+function stickyNavigation() {
+  if (navBar.offsetTop <= window.scrollY) {
+    navBar.style.top = window.scrollY + 'px';
   } else {
-    navBar.style.position = 'relative';
     navBar.style.top = '240px';
   }
 }
 
-//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_sticky
+window.addEventListener('scroll', stickyNavigation);
